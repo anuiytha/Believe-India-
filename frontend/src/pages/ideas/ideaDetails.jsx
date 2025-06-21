0832e8a1c0b46f1a347af446f4eb63f6fd8231ed
221910310008 <221910310008@gitam.in>
2025-06-20 22:19:17 -0400

    Fixed style.css issue in ideaDetails

--- frontend/src/pages/ideas/ideaDetails.jsx
+++ frontend/src/pages/ideas/ideaDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import "./styles.css"; // Import styles
+import "../../styles.css";
+
 
 const IdeaDetails = () => {
     const { id } = useParams();
