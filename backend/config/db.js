2e34aa6378e7554f35d18f33bf0e218df931587b
221910310008 <221910310008@gitam.in>
2025-06-21 01:05:44 -0400

    changed creds to .env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('BelieveIndia_DB', 'postgres', '2001', {
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
 });
 
 module.exports = sequelize;
--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ app.use('/', userRoutes);
 app.use('/', serviceRoutes);
 
 sequelize.sync().then(() => {
-    app.listen(3001, () => console.log('Server running on port 3001'));
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => console.log('Server running on port 3001'));
 })
 
 module.exports = app;
