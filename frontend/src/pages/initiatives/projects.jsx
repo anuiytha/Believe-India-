6420566412ddfaa2c8d1e7ae744faa4a36b78a91
221910310008 <221910310008@gitam.in>
2025-06-20 22:30:51 -0400

    Fixed contentful import issue in projects

--- frontend/src/pages/initiatives/projects.jsx
+++ frontend/src/pages/initiatives/projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../../styles.css";
-import useContentful from "../../contentful/useContentful";
+import useContentful from "../../../contentful/useContentful";
 
 
 const ProjectGrid = () => {
