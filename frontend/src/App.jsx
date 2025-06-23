9288e72f38e5ca6df5ed59a6c0b513d81994a9a7
221910310008 <221910310008@gitam.in>
2025-06-22 20:48:31 -0400

    Added Partners page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/navbar';
 import BiIdeas from './pages/ideas/bi_ideas';
@@ -19,10 +19,11 @@ import Current from './pages/initiatives/current';
 import Completed from './pages/initiatives/completed';
 import Upcoming from './pages/initiatives/upcoming';
 import SuccessStories from './pages/initiatives/success-stories';
+import Partners from "./pages/partners/partners";
+
 
 const App = () => {
   // const [message, setMessage] = useState('')
-
 
   // useEffect(() => {
   //   // Fetch data from backend
@@ -36,29 +37,28 @@ const App = () => {
     <div className="app">
       <Navbar />
       <main className="app-main">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/bi-ideas" element={<BiIdeas />} />
-            <Route path="/idea-form" element={<IdeaForm />} />
-            <Route path="/idea-bank" element={<IdeaBank />} />
-            <Route path="/idea/:id" element={<IdeaDetails />} />
-            <Route path="/update-idea/:ideaId" element={<UpdateIdea />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/services/consulting" element={<Consulting />} />
-            <Route path="/services/training" element={<Training />} />
-            <Route path="/services/workshops" element={<Workshops />} />
-            <Route path="/services/mentorship" element={<Mentorship />} />
-            <Route path="/services/research" element={<Research />} />
-            <Route path="/projects/current" element={<Current />} />
-            <Route path="/projects/completed" element={<Completed />} />
-            <Route path="/projects/upcoming" element={<Upcoming />} />
-            <Route path="/projects/success-stories" element={<SuccessStories />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bi-ideas" element={<BiIdeas />} />
+          <Route path="/idea-form" element={<IdeaForm />} />
+          <Route path="/idea-bank" element={<IdeaBank />} />
+          <Route path="/idea/:id" element={<IdeaDetails />} />
+          <Route path="/update-idea/:ideaId" element={<UpdateIdea />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/services/consulting" element={<Consulting />} />
+          <Route path="/services/training" element={<Training />} />
+          <Route path="/services/workshops" element={<Workshops />} />
+          <Route path="/services/mentorship" element={<Mentorship />} />
+          <Route path="/services/research" element={<Research />} />
+          <Route path="/projects/current" element={<Current />} />
+          <Route path="/projects/completed" element={<Completed />} />
+          <Route path="/projects/upcoming" element={<Upcoming />} />
+          <Route path="/projects/success-stories" element={<SuccessStories />} />
+          <Route path="/partners" element={<Partners />} />
 
+        </Routes>
       </main>
     </div>
   )
