b387311aaabb634ec1e8f1dffd5cb814d3ba8c85
221910310008 <221910310008@gitam.in>
2025-06-23 13:53:13 -0400

    Fixed image path

--- frontend/src/pages/campaigns/campaigns.jsx
+++ frontend/src/pages/campaigns/campaigns.jsx
@@ -4,14 +4,13 @@ import "../../styles.css";
 const Campaigns = () => {
     return (
         <>
-
             <div className="home-container">
                 <h1>Our Campaigns</h1>
                 <div className="home-grid">
                     <div className="home-card">
                         <h2>Podium Finish Campaign</h2>
                         {/* <p>Support the Indian Olympic Team with PodiumFinish</p> */}
-                        <img src='./podiumFinish.png' className="idea-image" />
+                        <img src='/podiumFinish .png' className="idea-image" />
                         <p>Podium Finish Campaign</p>
                         <p>To support Indian athletes for Olympics-2012</p>
                         <p>Signature Campaign: collected 50,000 signature & submitted to the govt.authority</p>
