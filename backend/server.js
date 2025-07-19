09b06c2bdf0d27c280274a6d1b75f03df49912d1
221910310008 <221910310008@gitam.in>
2025-07-19 19:50:15 -0400

    Changed the posting link in Register

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 app.use('/', ideaRoutes);
 app.use('/', projectRoutes);
-app.use('/api/auth', userRoutes);
+app.use('/', userRoutes);
 app.use('/', serviceRoutes);
 
 sequelize.sync().then(() => {
