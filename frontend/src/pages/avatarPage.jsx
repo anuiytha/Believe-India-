e60a4a7c36d59b2c898270208f8b07709225f3d6
221910310008 <221910310008@gitam.in>
2025-07-23 17:59:44 -0400

    Added Avatar

--- frontend/src/pages/avatarPage.jsx
+++ frontend/src/pages/avatarPage.jsx
@@ -1,13 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "../components/experience";
 
-const AvatarPage = () => {
+function App() {
     return (
         <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
-            <color attach="background" args={["#000000"]} />
+            <color attach="background" args={["#ececec"]} />
             <Experience />
         </Canvas>
-    )
+    );
 }
 
-export default AvatarPage;
+export default App;
