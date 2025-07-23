7d22d8ab53c51e00219c30577cd01f08d1a72184
221910310008 <221910310008@gitam.in>
2025-07-23 18:37:36 -0400

    Added Avatar

--- frontend/src/pages/avatarPage.jsx
+++ frontend/src/pages/avatarPage.jsx
@@ -3,7 +3,7 @@ import { Experience } from "../components/experience";
 
 function App() {
     return (
-        <Canvas camera={{ position: [0, 2, 5], fov: 30 }}>
+        <Canvas camera={{ position: [0, 2, 5], fov: 30 }} style={{ width: '100vw', height: '100vh' }}>
             <color attach="background" args={["#ececec"]} />
             <Experience />
         </Canvas>
