0da80e437376e09ac8c73f7a6d902893b92e83fa
221910310008 <221910310008@gitam.in>
2025-07-23 17:54:37 -0400

    Added Avatar

--- frontend/src/components/experience.jsx
+++ frontend/src/components/experience.jsx
@@ -6,7 +6,7 @@ export const Experience = () => {
         <>
             <OrbitControls />
             <Sky />
-            <Environment preset="sunset" />
+            {/* <Environment preset="sunset" /> */}
             <group position-y={-1}>
                 <ContactShadows opacity={1} scale={10} blur={1} far={10} resolution={256} color="#000000" />
                 <Avatar />
