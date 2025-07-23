313702a56ae877a57309bff9e81486e024aea0e9
221910310008 <221910310008@gitam.in>
2025-07-23 18:06:40 -0400

    Added Avatar

--- frontend/src/components/experience.jsx
+++ frontend/src/components/experience.jsx
@@ -5,13 +5,13 @@ import { MeshStandardMaterial } from "three";
 
 export const Experience = () => {
 
-    const { animation } = useControls({
-        animation: {
-            value: "Typing",
-            options: ["Typing", "Falling", "Standing"]
-        },
+    // const { animation } = useControls({
+    //     animation: {
+    //         value: "Typing",
+    //         options: ["Typing", "Falling", "Standing"]
+    //     },
 
-    });
+    // });
     return (
         <>
             <OrbitControls />
@@ -19,7 +19,7 @@ export const Experience = () => {
             {/* <Environment preset="sunset" /> */}
             <group position-y={-1}>
                 <ContactShadows opacity={1} scale={10} blur={1} far={10} resolution={256} color="#000000" />
-                <Avatar animation={animation} />
+                {/* <Avatar animation={animation} />
                 {
                     animation === "Typing" && (
                         <mesh scale={[0.8, 0.5, 0.8]} position-y={0.25}>
@@ -27,7 +27,7 @@ export const Experience = () => {
                             <meshStandardMaterial color="white" />
                         </mesh>
                     )
-                }
+                } */}
 
                 <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
                     <planeGeometry />
